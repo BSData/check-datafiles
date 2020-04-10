1cc5f5b1c1babc95ce9f70e6a606715da47a2f89
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-04-10 12:16:52 +0200

    fix: Set output even if no files

--- action.ps1
+++ action.ps1
@@ -2,6 +2,11 @@
 
 $ErrorActionPreference = 'Stop'
 
+# read inputs, set output
+$stagingPath = Get-ActionInput staging-path -Required
+$null = New-Item $stagingPath -ItemType Directory -ErrorAction:Ignore
+Set-ActionOutput staging-path $stagingPath
+
 # check if there are any cat/gst files to process, otherwise short-circuit out
 if ((Get-ChildItem -Recurse -Include *.cat, *.gst -File).Length -eq 0) {
     Write-Host "No datafiles to process." -ForegroundColor Green
@@ -15,6 +20,7 @@ function PrintAndInvoke{
     Write-Host $command -ForegroundColor Cyan
     Invoke-Expression $command
 }
+
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
 $env:DOTNET_NOLOGO = 1
 
@@ -23,11 +29,6 @@ $wham = "$PSScriptRoot/lib/wham"
 if ($null -eq (Get-Command $wham -ErrorAction SilentlyContinue)) {
     PrintAndInvoke "dotnet tool install wham --version 0.7.0 --tool-path ""$PSScriptRoot/lib"""
 }
-
-# read inputs, set output
-$stagingPath = Get-ActionInput staging-path -Required
-$null = New-Item $stagingPath -ItemType Directory -ErrorAction:Ignore
-Set-ActionOutput staging-path $stagingPath
 
 # TODO sometime in future "wham build/ci"
 
